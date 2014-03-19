a14266216b0ab38945521028d6e553ce3752b234
Thiago <thiago.anunciacao@me.com>
2014-03-19 15:56:37 -0300

    update

--- rails/recipes/dotenv.rb
+++ rails/recipes/dotenv.rb
@@ -4,8 +4,6 @@ node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
   execute "restart Rails app #{application}" do
-    command "rm -f #{deploy[:deploy_to]}/current/.env; ln -s #{deploy[:deploy_to]}/shared/.env #{deploy[:deploy_to]}/current"
-
     cwd deploy[:current_path]
     command node[:opsworks][:rails_stack][:restart_command]
     action :nothing
